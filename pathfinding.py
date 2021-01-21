a1c857d524026b1a1e057ff4034eefabc95c6934
S. Alex Yun <52643131+salexyun@users.noreply.github.com>
2021-01-21 11:12:04 -0500

    Added default cost for WeightedGridworld

--- pathfinding.py
+++ pathfinding.py
@@ -1,9 +1,5 @@
 # @author: S. Alex Yun
 # A collection of pathfinding algorithms.
-#
-# Adapted from: https://www.redblobgames.com/pathfinding/a-star/
-# - Copyright 2014 Red Blob Games <redblobgames@gmail.com>
-# - License: Apache v2.0 <https://www.apache.org/licenses/LICENSE-2.0>
 
 try:
     import heapq
@@ -19,21 +15,16 @@ except ImportError as err:
 
 
 class Gridworld:
-    """A two-dimensional (m x n) lattice graph."""
+    """
+    A two-dimensional (m x n) lattice graph.
+    Adapted from: https://www.redblobgames.com/pathfinding/a-star
+    """
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.source = None
-        self.goal = None
+        self.source = ()
+        self.goal = ()
         self.obstacles = []
-
-    def __repr__(self):
-        from pprint import pformat
-        board = [[0 for y in range(self.height)] for x in range(self.width)]
-        if self.obstacles:
-            for obstacle in self.obstacles:
-                board[obstacle[1]][obstacle[0]] = 1
-        return pformat(board, indent=4)
 
     def in_bounds(self, id):
         (x, y) = id
@@ -123,6 +114,7 @@ def dijkstra(graph, source_node, goal_node):
     return visited, cost
 
 
+
 def heuristic(cur_node, goal_node):
     """
     An utility function used in a_star.
@@ -137,7 +129,8 @@ def heuristic(cur_node, goal_node):
 def a_star(graph, source_node, goal_node):
     """
     A* search is a type of best-first search and
-    can be viewed as an extension of Dijkstra's algorithm."""
+    can be viewed as an extension of Dijkstra's algorithm.
+    """
     visited = {source_node: None}
     cost = defaultdict(lambda: math.inf)
     cost[source_node] = 0
@@ -155,7 +148,7 @@ def a_star(graph, source_node, goal_node):
                 cost[next_node] = alt_cost
                 priority = alt_cost + heuristic(next_node, goal_node)
                 heapq.heappush(frontier_pq, (priority, next_node))
-    
+
     return visited, cost
 
 
@@ -177,7 +170,10 @@ SQUARE_SIZE = 20
 
 
 def set_source(screen, gridworld):
-    """Initialize the source node as a Rect object."""
+    """
+    Initializes the source node in the Gridworld and
+    returns it as a Pygame Rect object (to be displayed onto the screen).
+    """
     gridworld.source = (0, 0)
     source_rect = pg.Rect([gridworld.source[0]*SQUARE_SIZE, gridworld.source[1]*SQUARE_SIZE,
                             SQUARE_SIZE, SQUARE_SIZE])
@@ -186,7 +182,10 @@ def set_source(screen, gridworld):
 
 
 def set_goal(screen, gridworld):
-    """Initialize the goal node as a Rect object."""
+    """
+    Initializes the goal node in the Gridworld and
+    returns it as a Pygame Rect object (to be displayed onto the screen).
+    """
     gridworld.goal = (gridworld.width-1, gridworld.height-1)
     goal_rect = pg.Rect([gridworld.goal[0]*SQUARE_SIZE, gridworld.goal[1]*SQUARE_SIZE,
                         SQUARE_SIZE, SQUARE_SIZE])
@@ -195,7 +194,7 @@ def set_goal(screen, gridworld):
 
 
 def draw_grid(screen, gridworld, source_rect, goal_rect):
-    """Draw the gridworld, source node, and goal node on the display."""
+    """Draws the Gridworld, source Rect, and goal Rect onto the screen."""
     for x in range(gridworld.width):
         for y in range(gridworld.height):
             pg.draw.rect(screen, pg.Color('white'),
@@ -206,7 +205,7 @@ def draw_grid(screen, gridworld, source_rect, goal_rect):
 
 
 def draw_obstacles(screen, gridworld):
-    """Draw an obstacle when a square is clicked."""
+    "Draws an obstacle when a square in the Gridworld is clicked."""
     for obstacle in gridworld.obstacles:
         pg.draw.rect(screen, pg.Color('red'),
                     [obstacle[0]*SQUARE_SIZE, obstacle[1]*SQUARE_SIZE,
@@ -214,7 +213,11 @@ def draw_obstacles(screen, gridworld):
 
 
 def remove_obstacles(screen, obstacles_removed):
-    """If an obstacle already exists and is clicked, it removes the obstacle."""
+    """
+    Opposite of draw_obstacles(): if an obstacle already exists
+    (i.e., sqaure is coloured), clicking this square
+    removes the obstacle and "uncolours" the square.
+    """
     for obstacle in obstacles_removed:
         pg.draw.rect(screen, pg.Color('white'),
                     [obstacle[0]*SQUARE_SIZE, obstacle[1]*SQUARE_SIZE,
@@ -222,7 +225,7 @@ def remove_obstacles(screen, obstacles_removed):
 
 
 def draw_visited(screen, gridworld, visited):
-    """Draw all squares that the stated algorithm visited."""
+    """Draws all squares that the stated algorithm visited."""
     for coordinates in visited:
         if coordinates != gridworld.source and coordinates != gridworld.goal:
             pg.draw.rect(screen, pg.Color('orange'),
@@ -232,7 +235,7 @@ def draw_visited(screen, gridworld, visited):
 
 # TODO: draw a line instead of a square.
 def draw_path(screen, gridworld, path):
-    """Draw the path that the stated algorithm found."""
+    """Draws the path that the stated algorithm found."""
     for coordinates in path:
         if coordinates != gridworld.source and coordinates != gridworld.goal:
             pg.draw.rect(screen, pg.Color('yellow'),
@@ -241,14 +244,14 @@ def draw_path(screen, gridworld, path):
 
 
 def get_grid_pos(mouse_x, mouse_y):
-    """Get the grid position from the mouse position."""
+    """Gets the grid position from the mouse position."""
     grid_x = mouse_x // SQUARE_SIZE
     grid_y = mouse_y // SQUARE_SIZE
     return grid_x, grid_y
 
 
 def main():
-    # Initialize screen and set title
+    # Initialize the screen and set the window caption
     pg.init()
     pg.display.set_caption("Pathfinding Visualizer")
 
@@ -264,9 +267,11 @@ def main():
                                                 text="Start", manager=manager)
     reset_button = pygame_gui.elements.UIButton(relative_rect=pg.Rect((680, 480), (100, 30)),
                                                 text="Reset", manager=manager)    
-                                                 
-    # Initialize the grid along with the source node and the goal node.
-    gridworld = Gridworld(GRID_SIZE[0]//SQUARE_SIZE, GRID_SIZE[1]//SQUARE_SIZE)
+
+    # Initialize the grid grid, source node, and goal node.
+    gridworld = WeightedGridworld(GRID_SIZE[0]//SQUARE_SIZE, GRID_SIZE[1]//SQUARE_SIZE)
+    gridworld.weights = {(x, y): 1 for x in range(gridworld.width) for y in range(gridworld.height)}
+
     source_rect, source_dragging = set_source(background, gridworld)
     goal_rect, goal_dragging = set_goal(background, gridworld)
     
@@ -274,6 +279,7 @@ def main():
     start = False
     obstacles_dragging = False
     obstacles_removed = []
+
     clock = pg.time.Clock() # for controlling the frame rate
 
     while True:
@@ -284,15 +290,15 @@ def main():
 
             elif event.type == pg.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = event.pos
-                if source_rect.collidepoint(event.pos): # move source node
+                if source_rect.collidepoint(event.pos): # source clicked to be moved
                     source_dragging = True
                     source_offset_x = source_rect.x - mouse_x
                     source_offset_y = source_rect.y - mouse_y
-                elif goal_rect.collidepoint(event.pos): # move goal node
+                elif goal_rect.collidepoint(event.pos): # goal clicked to be moved
                     goal_dragging = True
                     goal_offset_x = goal_rect.x - mouse_x
                     goal_offset_y = goal_rect.y - mouse_y
-                else: # create obstacles
+                else: # empty square clicked -> create obstacles
                     x, y = get_grid_pos(mouse_x, mouse_y)
                     if (x, y) not in gridworld.obstacles:
                         gridworld.obstacles.append((x, y))
@@ -305,6 +311,7 @@ def main():
                     obstacles_offset_x = obstacles_rect.x - mouse_x
                     obstacles_offset_y = obstacles_rect.y - mouse_y
             
+            # source, goal, or obstacle being dragged into new position; update position accordingly.
             elif event.type == pg.MOUSEMOTION:
                 mouse_x, mouse_y = event.pos
                 if source_dragging:
@@ -334,8 +341,10 @@ def main():
                 elif event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                     if event.ui_element == start_button:
                         start = True
-                    elif event.ui_element == reset_button:
-                        gridworld = Gridworld(GRID_SIZE[0]//SQUARE_SIZE, GRID_SIZE[1]//SQUARE_SIZE)
+                    elif event.ui_element == reset_button: # wipe out the grid
+                        gridworld = WeightedGridworld(GRID_SIZE[0]//SQUARE_SIZE, GRID_SIZE[1]//SQUARE_SIZE)
+                        gridworld.weights = {(x, y): 1 for x in range(gridworld.width)
+                                                    for y in range(gridworld.height)}
                         source_rect, source_dragging = set_source(screen, gridworld)
                         goal_rect, goal_dragging = set_goal(screen, gridworld)
                         start = False
@@ -343,11 +352,12 @@ def main():
             manager.process_events(event)
         manager.update(time_delta)
 
+        # Set up the screen, background, and the GUI.
         screen.fill(pg.Color('grey'))
         background.fill(pg.Color('grey'))
         manager.draw_ui(screen)
 
-        # Draw the grid with source and goal nodes, and then obstacles.
+        # Actually draw the grid with source, node, and obstacles onto the screen.
         draw_grid(background, gridworld, source_rect, goal_rect)
         draw_obstacles(background, gridworld)
         remove_obstacles(background, obstacles_removed)
@@ -359,9 +369,10 @@ def main():
             if algo_chosen == "DFS":
                 visited = dfs(gridworld, gridworld.source, gridworld.goal)
             elif algo_chosen == "Dijkstra":
-                pass
+                visited, cost_dijkstra = dijkstra(gridworld, gridworld.source, gridworld.goal)
             elif algo_chosen == "A*":
-                pass
+                visited, cost_a_star = a_star(gridworld, gridworld.source, gridworld.goal)
+
             path = reconstruct_path(visited, gridworld.source, gridworld.goal)
             draw_visited(background, gridworld, visited)
             draw_path(background, gridworld, path)
